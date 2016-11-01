22c56050c21e36be0779578d0c1edd1a20ba0440
Olav Frengstad <olav@fwt.no>
2016-11-01 18:35:20 +0100

    NotFound: Add support for adding a sidebar through props

--- lib/pages/NotFound.jsx
+++ lib/pages/NotFound.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 import {Grid, Row, Col} from 'react-bootstrap'
 export const NotFound  = (props) => {
    console.log('render', 'pages/NotFound', window.location.hash)
-   console.log(props)
+
+   let {sidebar} = props
+
    return (
       <Grid>
          <Row>
-            <Col xs={12} className="text-center">
+            {sidebar && <Col xs={4}>{sidebar}</Col>}
+            <Col xs={sidebar ? 8 : 12} className="text-center">
                <h1>File not found [404]</h1>
 
                <p className="lead">
