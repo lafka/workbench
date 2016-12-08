4f25a2df24c189f598db0fc307198483b73fa2ed
Olav Frengstad <olav@fwt.no>
2016-12-08 14:52:56 +0100

    Migrate setup-guide to use antd
    Antd is a much more elegant library for working with style

--- lib/bundle/dashboard/network/Overview.jsx
+++ lib/bundle/dashboard/network/Overview.jsx
@@ -1,10 +1,66 @@
 import React from 'react'
 
-import {SetupSteps} from '../setup-guide'
+import {SetupGuide, SetupSteps} from '../setup-guide'
 
-export const Overview = ({network, ...props}) =>
-   <div>
-      <SetupSteps link={true} network={network} {...props} />
+export class Overview extends React.Component {
+   constructor(props) {
+      super(props)
 
-      Why am i not a dashboard?
-   </div>
+      this.ignoreSetup = this.ignoreSetup.bind(this)
+      this.gotoSetup = this.gotoSetup.bind(this)
+   }
+
+   ignoreSetup() {
+      SetupGuide.skipSetup(this.props.network)
+      this.forceUpdate()
+   }
+
+   gotoSetup() {
+      let
+         {router, params, routes} = this.props,
+         newRoutes = _.slice(routes, 0, -1).concat([{path: 'setup'}]),
+         nextPath = _.map(newRoutes, r => r.path)
+                     .join('/')
+                     .replace('//', '/'),
+         url
+
+      url = _.reduce(params, (acc, v, k) => acc.replace(":" + k, v), nextPath)
+
+      router.push(url)
+   }
+
+   render() {
+      let {network, ...props} = this.props
+
+      return (
+         <div>
+            <div
+               style={{marginBottom: '1rem',
+                       display: SetupGuide.setupFinished(network) ? 'none' : 'inherit',
+                     }}>
+               <div>
+                  <button
+                     onClick={this.ignoreSetup}
+                     type="button"
+                     className="close">
+                        <span style={{fontSize: '12px', verticalAlign: 'middle'}}>Ignore</span>
+                        &nbsp;
+                        <span aria-hidden="true">&times;</span>
+                  </button>
+               </div>
+
+               <div
+                  onClick={this.gotoSetup}
+                  style={{clear: 'both', marginTop: '1rem', opacity: 0.5}}>
+
+                  <SetupSteps link={true} network={network} {...props} />
+               </div>
+            </div>
+
+            <pre>
+               {JSON.stringify(network, null, 2)}
+            </pre>
+         </div>
+      )
+   }
+}
